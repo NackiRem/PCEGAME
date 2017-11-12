12f5bed3f134f904c162ad3987fad209159b5636
cjs <thss15_chenjs@163.com>
2017-11-13 01:52:53 +0800

    nothing

---
+++ PCE.asm
@@ -0,0 +1,22 @@
+.386
+.model flat, stdcall
+option casemap:none
+
+include			windows.inc
+include 		gdi32.inc 
+includelib		gdi32.lib
+include 		user32.inc 
+includelib		user32.lib
+include 		kernel32.inc 
+includelib 		kernel32.lib
+
+.data?
+hInstance 		dd		?
+hWinMain 		dd  	?
+
+.data
+
+
+
+.code
+_ProcWinMain 	proc
