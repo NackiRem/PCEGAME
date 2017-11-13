99ff6ceda10d12407728a30ac9f13c5344126692
cjs <thss15_chenjs@163.com>
2017-11-14 01:28:43 +0800

    Completed start render

--- PCE.asm
+++ PCE.asm
@@ -10,6 +10,15 @@ includelib		user32.lib
 include 		kernel32.inc 
 includelib 		kernel32.lib
 
+;---------------------------
+;equ等值定义
+;---------------------------
+IDB_START		equ 	100
+
+
+;---------------------------
+;数据段
+;---------------------------
 .data?
 hInstance 		dd		?
 hWinMain 		dd  	?
@@ -18,22 +27,84 @@ hWinMain 		dd  	?
 szClassName		db		'Myclass', 0
 szCaptionMain	db		'My first Window!', 0
 szText			db		'Win32 Assembly, Simple and powerful !', 0
+gameStatus		dd 		0
 
+.const
+WINDOWS_X		dd 		100
+WINDOWS_Y		dd 		100
+WINDOWS_WIDTH	dd		600
+WINDOWS_HEIGHT	dd		400
 
+;---------------------------
+;代码段
+;---------------------------
 .code
-_ProcWinMain 	proc	uses ebx edi esi, hWnd, uMsg,wParam,lParam
+_InitWindows	proc 	hWnd, wParam, lParam
+		invoke	InvalidateRect, hWnd, NULL, FALSE
+		ret
+_InitWindows 	endp
+
+
+_StartRender proc uses ebx edi esi, hWnd
+		local	@stPs:PAINTSTRUCT, @hdc, @hdcBmp, @hdcBuffer
+		local	@cptBmp, @m_hStartBmp
+		pushad
+
+		invoke 	BeginPaint, hWnd, addr @stPs
+		mov 	@hdc, eax
+		invoke	CreateCompatibleBitmap, @hdc, WINDOWS_WIDTH, WINDOWS_HEIGHT
+		mov		@cptBmp, eax
+		invoke	CreateCompatibleDC, @hdc
+		mov 	@hdcBmp, eax
+		invoke	CreateCompatibleDC, @hdc
+		mov		@hdcBuffer, eax
+		invoke 	LoadBitmap, hInstance, IDB_START
+		mov		@m_hStartBmp, eax
+
+		invoke	SelectObject, @hdcBuffer, @cptBmp
+		invoke 	SelectObject, @hdcBmp, @m_hStartBmp
+		invoke 	BitBlt, @hdcBuffer, 0, 0, WINDOWS_WIDTH, WINDOWS_HEIGHT, @hdcBmp, 0, 0, SRCCOPY
+		invoke	BitBlt, @hdc, 0, 0, WINDOWS_WIDTH, WINDOWS_HEIGHT, @hdcBuffer, 0, 0, SRCCOPY
+
+		invoke	DeleteObject, @cptBmp
+		invoke 	DeleteObject, @m_hStartBmp
+		invoke	DeleteDC, @hdcBuffer
+		invoke 	DeleteDC, @hdcBmp
+
+		invoke 	EndPaint, hWnd, addr @stPs
+		popad
+		ret
+_StartRender endp
+
+
+Render proc uses ebx edi esi, hWnd
+		ret
+Render endp
+
+
+OverRender proc uses ebx edi esi, hWnd
+		ret
+OverRender endp
+
+
+_ProcWinMain 	proc	uses ebx edi esi ebp, hWnd, uMsg,wParam,lParam
 		local	@stPs:PAINTSTRUCT
 		local	@stRect:RECT
 		local	@hDc
 		
 		mov	eax,uMsg
-		.if	eax == WM_PAINT
-			invoke	BeginPaint, hWnd, addr @stPs
-			mov	@hDc, eax
+		.if eax == WM_CREATE
+			invoke	_InitWindows, hWnd, wParam, lParam
+		.elseif	eax == WM_PAINT
+			.if gameStatus == 0
+				invoke _StartRender, hWnd
+			.endif
+			; invoke	BeginPaint, hWnd, addr @stPs
+			; mov	@hDc, eax
 
-			invoke	GetClientRect, hWnd, addr @stRect
-			invoke	DrawText, @hDc, addr szText,-1, addr @stRect, DT_SINGLELINE or DT_CENTER or DT_VCENTER
-			invoke	EndPaint, hWnd, addr @stPs
+			; invoke	GetClientRect, hWnd, addr @stRect
+			; invoke	DrawText, @hDc, addr szText,-1, addr @stRect, DT_SINGLELINE or DT_CENTER or DT_VCENTER
+			; invoke	EndPaint, hWnd, addr @stPs
 		.elseif	eax == WM_CLOSE
 			invoke	DestroyWindow, hWinMain
 			invoke	PostQuitMessage, NULL
@@ -69,7 +140,7 @@ _WinMain	proc
 ;-----------------------
 ;建立并显示窗口
 ;-----------------------
-		invoke	CreateWindowEx, WS_EX_CLIENTEDGE, offset szClassName, offset szCaptionMain, WS_OVERLAPPEDWINDOW, 100, 100, 600, 400, NULL, NULL, hInstance, NULL
+		invoke	CreateWindowEx, WS_EX_CLIENTEDGE, offset szClassName, offset szCaptionMain, WS_OVERLAPPEDWINDOW, WINDOWS_X, WINDOWS_Y, WINDOWS_WIDTH, WINDOWS_HEIGHT, NULL, NULL, hInstance, NULL
 		mov 	hWinMain, eax
 		invoke	ShowWindow, hWinMain, SW_SHOWNORMAL
 		invoke 	UpdateWindow, hWinMain
@@ -81,7 +152,7 @@ _WinMain	proc
 			.break	.if eax == 0
 			invoke 	TranslateMessage, addr @stMsg
 			invoke 	DispatchMessage, addr @stMsg
-			.endw
+		.endw
 		ret
 _WinMain 	endp
 
