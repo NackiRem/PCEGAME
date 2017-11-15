1f3d54e2fac87a1295828f1fdadb1dfafe25d85a
cjs <thss15_chenjs@163.com>
2017-11-15 09:53:17 +0800

    Completed part of create

--- PCE.asm
+++ PCE.asm
@@ -13,7 +13,15 @@ includelib 		kernel32.lib
 ;---------------------------
 ;equ等值定义
 ;---------------------------
-IDB_START		equ 	100
+IDB_START			equ 	100
+IDB_BACKGROUND  	equ 	101
+IDB_BLUE_BRICK  	equ  	102
+IDB_GREEN_BRICK  	equ  	103
+IDB_RED_BRICK  		equ  	104
+IDB_STONE_BRICK  	equ 	105
+IDB_YELLOW_BRICK  	equ  	106
+IDB_HERO  			equ 	107
+
 WINDOWS_WIDTH	equ 	500
 WINDOWS_HEIGHT 	equ 	700
 
@@ -64,26 +72,104 @@ Brick ends
 .data?
 hInstance 		dd		?
 hWinMain 		dd  	?
+;位图句柄
+;---------------------------
+m_hBackBmp				dd 	?
+m_hHeroBmp				dd 	?
+m_hBlueBlockBmp			dd 	?
+m_hGreenBlockBmp		dd 	?
+m_hPurpleBlockBmp		dd 	?
+m_hStoneBlockBmp		dd 	?
+m_hYellowBlockBmp		dd 	?
+
+
 
 .data
 szClassName		db		'Myclass', 0
 szCaptionMain	db		'My first Window!', 0
 
 ;全局变量：
-;gameStatus: 0--游戏开始界面, 1--游戏中，2--游戏结束
-gameStatus		dd 		0
+;GameState: 0--游戏开始界面, 1--游戏中，2--游戏结束
+GameState		dd 		0
 TotalGrade  	dd  	0
 MapStartX  		dd      -125
 MapStartY  		dd  	300
+m_brickBmpNames dd  IDB_BLUE_BRICK, IDB_RED_BRICK, IDB_GREEN_BRICK, IDB_STONE_BRICK, IDB_YELLOW_BRICK
+
+m_hero 			Hero 	<>
+m_Map 			Brick 	10 	DUP(<>)
+
 
 ;---------------------------
 ;代码段
 ;---------------------------
 .code
-_InitWindows	proc 	hWnd, wParam, lParam
+CreateHero proc
+  pushad
+  ;mov m_hero.hBmp, m_HeroBmp
+  mov eax, m_HeroBmp
+  mov m_hero.hBmp, eax
+  mov m_hero.pos.x, 250
+  mov m_hero.pos.y, 235
+  mov m_hero.h_size.x, HERO_SIZE_X
+  mov m_hero.h_size.y, HERO_SIZE_Y
+  mov m_hero.curFrameIndex, 0
+  mov m_hero.maxFrameSize, HERO_MAX_FRAME_NUM
+  mov m_hero.speed_x, 0
+  mov m_hero.speed_y, 0
+  mov m_hero.g, 1
+  popad
+  ret
+CreateHero endp
+
+CreateMap proc
+  pushad
+  mov ecx, 20
+  xor esi, esi
+    L1:
+      push ecx
+      mov ecx, 10
+      xor ebx, ebx
+        L2:
+          mov ax, RowSize
+          mul esi
+          add eax, ebx
+          ;mov eax,m_Map[eax]
+          ;call WriteInt
+          mov edx, m_BrickBmp[0]
+          mov m_Map[eax].hBmp, edx
+          
+
+          xor eax, eax
+          add ebx, TYPE Brick
+        LOOP L2
+        add esi, 1
+       pop ecx
+      LOOP L1
+  popad
+  ret
+CreateMap endp
+
+Init	proc 	uses ebx edi esi, hWnd, wParam, lParam
+		invoke	LoadBitmap, hInstance, IDB_BACK
+		mov 	m_hBackBmp, eax
+		invoke	LoadBitmap, hInstance, IDB_BRICK_BLUE
+		mov 	m_hBlueBlockBmp, eax
+		invoke	LoadBitmap, hInstance, IDB_BRICK_GREEN
+		mov 	m_hGreenBlockBmp, eax
+		invoke	LoadBitmap, hInstance, IDB_BRICK_PURPLE
+		mov 	m_hPurpleBlockBmp, eax
+		invoke	LoadBitmap, hInstance, IDB_BRICK_STONE
+		mov 	m_hStoneBlockBmp, eax
+		invoke	LoadBitmap, hInstance, IDB_BRICK_YELLOW
+		mov 	m_hYellowBlockBmp, eax
+		invoke	LoadBitmap, hInstance, IDB_HERO
+		mov 	m_hHeroBmp, eax
+
+
 		invoke	InvalidateRect, hWnd, NULL, FALSE
 		ret
-_InitWindows 	endp
+Init 	endp
 
 
 _StartRender 	proc uses ebx edi esi, hWnd
@@ -119,6 +205,13 @@ _StartRender endp
 
 
 Render proc uses ebx edi esi, hWnd
+		local 	@stPs:PAINTSTRUCT, @hdc, @hdcBmp, @hdcBuffer
+		local 	@cptBmp
+		pushad
+
+
+
+		popad
 		ret
 Render endp
 
@@ -126,6 +219,51 @@ Render endp
 OverRender proc uses ebx edi esi, hWnd
 		ret
 OverRender endp
+
+
+KeyDown proc uses ebx edi esi, hWnd, wParam, lParam
+  .if wParam == VK_UP
+    ;人物位置改变
+    invoke InvalidateRect, hWnd, NULL, FALSE
+
+  .elseif wParam == VK_DOWN
+    ;人物位置改变
+    invoke InvalidateRect, hWnd, NULL, FALSE
+
+  .elseif wParam == VK_RIGHT
+    ;人物位置改变
+    invoke InvalidateRect, hWnd, NULL, FALSE
+
+  .elseif wParam == VK_LEFT
+    ;人物位置改变
+    invoke InvalidateRect, hWnd, NULL, FALSE
+
+  .endif
+  ret
+KeyDown endp
+
+
+KeyUp proc uses ebx edi esi, hWnd, wParam, lParam
+.if wParam == VK_UP
+  ;人物位置改变
+  invoke InvalidateRect, hWnd, NULL, FALSE
+
+.elseif wParam == VK_DOWN
+  ;人物位置改变
+  invoke InvalidateRect, hWnd, NULL, FALSE
+
+.elseif wParam == VK_RIGHT
+  ;人物位置改变
+  invoke InvalidateRect, hWnd, NULL, FALSE
+
+.elseif wParam == VK_LEFT
+  ;人物位置改变
+  invoke InvalidateRect, hWnd, NULL, FALSE
+
+.endif
+  ret
+KeyUp endp
+
 
 LButtonDown 	proc, 	hWnd, wParam, lParam
 		local	@ptMouse:POINT
@@ -136,14 +274,42 @@ LButtonDown 	proc, 	hWnd, wParam, lParam
 		invoke 	HIWORD, lParam
 		mov 	ptMouse, eax
 
-		.if  gameStatus == 0
-			mov 	gameStatus, 1
-
+		.if  GameState == 0
+			mov 	eax, 1
+			mov 	GameState, eax
+			invoke	Render, hWnd
 		.endif
 
 		popad
 		ret
 LButtonDown 	endp
+
+HeroUpdate proc
+ret
+HeroUpdate endp
+
+
+MapUpdate proc
+ret
+MapUpdate endp
+
+
+GameStateUpdate proc
+ret
+GameStateUpdate endp
+
+
+TimerUpdate proc uses ebx edi esi, hWnd, wParam, lParam
+  invoke HeroUpdate
+  invoke MapUpdate
+  invoke GameStateUpdate
+  invoke InvalidateRect, hWnd, NULL, FALSE
+
+  .if GameState == 2
+    invoke KillTimer, hWnd, TIMER_ID
+  .endif
+  ret
+TimerUpdate endp
 
 
 _ProcWinMain 	proc	uses ebx edi esi ebp, hWnd, uMsg,wParam,lParam
@@ -153,21 +319,23 @@ _ProcWinMain 	proc	uses ebx edi esi ebp, hWnd, uMsg,wParam,lParam
 		
 		mov	eax,uMsg
 		.if eax == WM_CREATE
-			invoke	_InitWindows, hWnd, wParam, lParam
+			invoke	Init, hWnd, wParam, lParam
 		.elseif	eax == WM_PAINT
-			.if gameStatus == 0
+			.if GameState == 0
 				invoke _StartRender, hWnd
-			.elseif gameStatus == 1
-
+			.elseif GameState == 1
+				invoke Render,hWnd
+			.elseif GameState == 2
+				invoke OverRender, hWnd
 			.endif
 		.elseif eax == WM_KEYDOWN
-
+			invoke KeyDown, hWnd, wParam, lParam
 		.elseif eax == WM_KEYUP
-
+			invoke KeyUp, hWnd, wParam, lParam
 		.elseif eax == WM_LBUTTONDOWN
-
+			invoke 	LButtonDown, hWnd, wParam, lParam
 		.elseif eax == WM_TIMER
-
+			invoke TimerUpdate, hWnd, wParam, lParam
 		.elseif	eax == WM_CLOSE
 			invoke	DestroyWindow, hWinMain
 			invoke	PostQuitMessage, NULL
